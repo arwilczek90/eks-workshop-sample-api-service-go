4fc95fa9af9b667b3e0f0bfc3f9f6471f50611f7
Adam Wilczek <arwilczek90@users.noreply.github.com>
2018-11-26 20:24:40 -0800

    cifhfkdhgodhc
    kfgbdkvhdeifighn

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Jesus Christ I am tired"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
